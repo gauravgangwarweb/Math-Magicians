f5524cf1048288ddcc0924339de79f30950073a1
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-16 17:51:00 +0530

    Add padding in nav component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="px-5 py-2 bg-yellow-600 text-black">
+    <div className="px-10 py-2 bg-yellow-600 text-black">
       <nav className="text-center flex items-center justify-between">
         <h1 className="text-red-600 text-4xl">Math Magicians</h1>
         <div className="text-lg flex gap-10">
