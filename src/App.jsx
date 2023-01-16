4c117878f1dda0b1a58819021aa21cdd6dd4ed13
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-16 16:35:11 +0530

    Modify App js file to add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Calculator from './components/Calculator.jsx';
-
-import React, { Component } from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './page/HomePage'
+import CalculatorPage from './page/CalculatorPage'
+import QuotePage from './page/QuotePage';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <Calculator />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/calculator' element={<CalculatorPage />} />
+        <Route path='/quote' element={<QuotePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
